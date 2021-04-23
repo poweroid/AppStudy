df691997781a6cdff12183ccbea76117aea9bb97
Dotton <dotton95@gmail.com>
2021-04-23 20:00:45 +0900

    [Dotton95] setting init

---
+++ Dotton95/AppStudy/app/src/main/java/com/example/studyapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.studyapp
+
+import android.content.Intent
+import android.os.Bundle
+import com.example.studyapp.bmi.BmiActivity
+import com.example.studyapp.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        initButton()
+    }
+
+    fun initButton() {
+        binding.btnBmi.setOnClickListener {
+            startActivity(Intent(this, BmiActivity::class.java))
+        }
+        binding.btnLotto.setOnClickListener {
+            // startActivity(Intent(this, LottoActivity::class.java))
+        }
+    }
+}
---
+++ Dotton95/AppStudy/app/src/main/java/com/example/studyapp/bmi/BmiActivity.kt
@@ -0,0 +1,15 @@
+package com.example.studyapp.bmi
+
+import android.os.Bundle
+import com.example.studyapp.BaseActivity
+import com.example.studyapp.R
+import com.example.studyapp.databinding.ActivityBmiBinding
+
+
+class BmiActivity : BaseActivity<ActivityBmiBinding>(R.layout.activity_bmi) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
